1799b7d9de32d102a67376b869002ef512d0f5fc
glebster-d <glebon_1@yahoo.com>
2020-06-11 21:05:29 +0300

    Fixing some issues and adding App class

---
+++ app/src/main/java/com/glebsterd/mytodolist/App.java
@@ -0,0 +1,45 @@
+package com.glebsterd.mytodolist;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
+
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setNotificationChannels();
+    }
+
+    // Set notification channel if api version greater or equals to 26
+    private void setNotificationChannels() {
+
+        // If OS version equals or greater than API 26, create notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            NotificationChannel channel = new NotificationChannel(
+                    getString(R.string.channel_id),
+                    getString(R.string.channel_name),
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+
+            channel.setDescription(getString(R.string.channel_description));
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            channel.setShowBadge(false);
+            channel.setBypassDnd(true);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+    }// setNotificationChannels
+}
